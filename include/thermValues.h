4ca15c712334c0d6314c89dc8e4b6b60cece0f66
jonas <wompusi@gmail.com>
2025-04-08 19:49:15 +1200

    almost going

--- include/thermValues.h
+++ include/thermValues.h
@@ -19,19 +19,19 @@
 #define BCOEFFICIENT 3960 //magic thermistor number
 #define NOMINAL_TEMPERATURE 25 //thermistor 30k at 25c
 #define TRIM_TEMP_ADC0  0 // factor to add to temp C for adc1
-#define TRIM_TEMP_ADC1  3 // factor to add to temp C for adc1
+#define TRIM_TEMP_ADC1  0 // factor to add to temp C for adc1
 
 //sleep time
 // got 7 days of battery with sleeptime 60 seconds
 //#define sleepTime 1  //1 seconds sleeptime
-#define sleepTime 20 //20secs sleeptime
+#define sleepTime 2 //20secs sleeptime
 //#define sleepTime 100 //10secs sleeptime
 //#define sleepTime 300  //60 seconds x 5
 //#define sleepTime 600  //60 seconds x 10
 //#define sleepTime 900  //60 seconds x 15
-#define looptimedelay 2     //5
+#define looptimedelay 2    //5
 #define SleeptimeUs (sleepTime*1000000)
-#define WATCHDOGtimeout  10// WDT timeout 10 seconds
+#define WATCHDOGtimeout  20// WDT timeout 10 seconds
 
 int Rtherm= 0;
 //int ADCMax = 32767;
--- src/BLEThermometer.cpp
+++ src/BLEThermometer.cpp
@@ -21,11 +21,20 @@
 
 // See the following for generating UUIDs:
 // https://www.uuidgenerator.net/
-#define SERVICE_poolFloater_UUID        "d586aca9-12be-4e03-8ad7-b0106ba6018f"
+#define SERVICE_poolFloater_UUID "d586aca9-12be-4e03-8ad7-b0106ba6018f"
 #define CHARACTERISTIC_tempC_UUID "42b5ebfe-a946-4f85-ba49-03495b0aa5ee"
-#define CHARACTERISTIC_setEpoch "setEpochNotUUID"
+// UUID for jonas epoch service with spare random characteristic
+#define SERVICE_EPOCH_uuid "1a10a61c-495e-4b63-91b7-48924d50b96e"
+#define CHARACTERISTIC_setEpoch "8a4fb731-ea09-4d0c-8fb8-164034997a82"
+#define CHARACTERISTIC_rand_characteristic_uuid "342810fb-0018-48bc-96a5-2d0de82fe297"
 
-BLECharacteristic *pCharacteristicValuesStructure; //global for the characterisy=tic, that way i can access it in loop
+
+BLECharacteristic pCharacteristicValuesStructure(CHARACTERISTIC_tempC_UUID, BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE | BLECharacteristic::PROPERTY_NOTIFY);	   // global for the characterisy=tic, that way i can access it in loop
+BLECharacteristic pCharacteristicValuesStructureEpoch(CHARACTERISTIC_setEpoch, BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE | BLECharacteristic::PROPERTY_NOTIFY); // global for the characterisy=tic, that way i can access it in loop
+
+BLEAdvertising *pAdvertising;
+BLEUUID tempC_UUID = BLEUUID(CHARACTERISTIC_tempC_UUID);
+BLEUUID setEpochUUID = BLEUUID(CHARACTERISTIC_setEpoch);
 
 //time stuff library import and globals....
 #include <ESP32Time.h>
@@ -38,8 +47,9 @@ int LED = 5; // LED connected to pin 2
 float oldWeight = 0.000; // will use this variable to print over text in screen in backgound colour to blank text before next write
 
 float newWeight = 0.000; //
-long count = 0; //will use this for testing mqtt persistence
-struct  DataSend {
+long count = 0;			 // will use this for testing mqtt persistence
+struct DataSend
+{
 	unsigned long epoch;
 	float tempC;
 	float tempCa;
@@ -62,24 +72,34 @@ CircularBuffer<DataSend, 110> RTC_NOINIT_ATTR bufferCircle ;
 //for BT address
 const uint8_t* point = esp_bt_dev_get_address();
 
+//prototypes
+void printBuffer(void);
+
+
+
 class ServersCallbacks: public BLEServerCallbacks {
 	void onConnect(BLEServer *pServer) {
+		esp_task_wdt_reset();
 		isConnected = true;
 		digitalWrite(LED, HIGH);
 		log_i("*********");
 		log_i("Co nected");
+		//printBuffer();
+		
 
 	}
 	void onDisconnect(BLEServer *pServer) {
+		esp_task_wdt_reset();
 		isConnected = false;
 		digitalWrite(LED, LOW);
 		log_i("*********");
 		log_i("Diss co nected: ");
+		pServer->getAdvertising()->start();
+		
 	}
 
 };
-
-class MyCallbacks: public BLECharacteristicCallbacks {
+class MyCallbacks2: public BLECharacteristicCallbacks {
 	void onWrite(BLECharacteristic *pCharacteristic) {
 		std::string value = pCharacteristic->getValue();
 		BLEUUID gotUUID = pCharacteristic->getUUID();
@@ -91,7 +111,7 @@ class MyCallbacks: public BLECharacteristicCallbacks {
 				log_d("bit index %i;",i);
 			}
 
-
+esp_task_wdt_reset();
 			log_d("*********");
 			String doing = value.c_str();
 			int length = doing.length();
@@ -123,7 +143,56 @@ log_i("hi there");
 	}
 	void onRead(BLECharacteristic *pCharacteristic) {
 		log_i(	"++++++++Read from client has just happened+++++++++++++++++++");
-printBuffer();
+esp_task_wdt_reset();
+	}
+};
+
+
+class MyCallbacks: public BLECharacteristicCallbacks {
+	void onWrite(BLECharacteristic *pCharacteristic) {
+		std::string value = pCharacteristic->getValue();
+		BLEUUID gotUUID = pCharacteristic->getUUID();
+		if (value.length() > 0) {
+			log_i("*********");
+			log_i("New value: ");
+			for (int i = 0; i < value.length(); i++){
+				log_d("uid bit %i;",value[i]);
+				log_d("bit index %i;",i);
+			}
+
+esp_task_wdt_reset();
+			log_d("*********");
+			String doing = value.c_str();
+			int length = doing.length();
+			log_i("epoch before trim %s",doing);
+			doing.remove((length - 3), 3); // trim mS from the epoch
+			log_i("epoch after trim %s",doing);
+			epoch = doing.toInt();
+
+			log_i("epochPrint %Ld",(long) epoch, 10);
+			if (epoch < 2524611600L && epoch > 946688400L) {
+
+				rtc.setTime(epoch, 0);
+				log_i("epochPrint in epoch >150.... %Ld commer 10 means nothing %d",(long) epoch, 10);
+
+			} //close if check
+		}
+		if (gotUUID.bitSize() > 0) {
+			std::string valueUUID = gotUUID.toString();
+			for (int i = 0; i < valueUUID.length(); i++){
+				log_d("UUID; %d",valueUUID[i]);}
+
+		}
+log_i("hi there");
+// nodered cant send giveTime when device is resetting all the tome, so under these circumstances, this function is useless
+		if (value == "giveTime") {
+			log_d("the time now is");
+			log_d("time %s",rtc.getDateTime());
+		}
+	}
+	void onRead(BLECharacteristic *pCharacteristic) {
+		log_i(	"++++++++Read from client has just happened+++++++++++++++++++");
+esp_task_wdt_reset();
 	}
 };
 
@@ -148,8 +217,8 @@ log_i("in print buffer function");
 		//serializeJson(doc, Serial);
 		log_i("string tpo send to node3red %s",tempString);
 
-		pCharacteristicValuesStructure->setValue(tempString);
-		pCharacteristicValuesStructure->notify();
+		pCharacteristicValuesStructure.setValue(tempString);
+		pCharacteristicValuesStructure.notify();
 		log_i("just after characheristic set value and notify %s",tempString);
 		delay(50);
 
@@ -213,8 +282,7 @@ float getTemperature(int count) {
 	steinhartVo0 += TRIM_TEMP_ADC0;
 	steinhartVo1 += TRIM_TEMP_ADC1;
 	log_d("voltsVo %fV, voltsVo1 %fV ,voltsVcc %fV.", voltsVo0, voltsVo1,  voltsVcc);
-	log_i("Temperature top %f C, Temperature bot %f C, rtherm %d Ohm, batt volt %fV", steinhartVo0, steinhartVo1, Rtherm,
-			voltsBat);
+	log_i("Temperature top %f C, Temperature bot %f C, rtherm %d Ohm, batt volt %fV", steinhartVo0, steinhartVo1, Rtherm, voltsBat);
 //put the battery voltage into the transmission structure
 	boing.battV = voltsBat;
 	boing.epoch = rtc.getEpoch();
@@ -240,10 +308,12 @@ void printDeviceAddress() {
     }
 
   }
+  Serial.print("\n");
  // reset_reason = esp_reset_reason();
 }
 
 void setup() {
+	
 	//sleep setup//////watchdog Timer///////////////////////////////////////////////////////////
 	uint64_t sleepies = (unsigned int)sleepTime * 1000000L;
 	esp_sleep_enable_timer_wakeup(sleepies);
@@ -258,28 +328,36 @@ void setup() {
 
 
 	//setup BLE setup BLE setup BLE setup BLE setup BLE setup BLE setup BLE setup BLE setup BLE setup BLE
-	log_d("Starting BLE work!");
+	log_i("Starting BLE work!----------------------------------------");
 	BLEDevice::init("PoolThermometer");
 	BLEServer *pServer = BLEDevice::createServer();
 	pServer->setCallbacks(new ServersCallbacks);
+
 	BLEService *pService = pServer->createService(SERVICE_poolFloater_UUID);
-	pCharacteristicValuesStructure = pService->createCharacteristic(CHARACTERISTIC_tempC_UUID,
-			BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE
-					| BLECharacteristic::PROPERTY_NOTIFY);
-	//pCharacteristicValuesStructure->setNotifyProperty(true);
-	//pCharacteristicValuesStructure->setValue("um");
-	pCharacteristicValuesStructure->setCallbacks(new MyCallbacks());
+	pService->addCharacteristic(&pCharacteristicValuesStructure);
+	pCharacteristicValuesStructure.setCallbacks(new MyCallbacks());
+
+
+// try and create Epochservice
+	BLEService *pService_EPOCH = pServer->createService(SERVICE_EPOCH_uuid);
+	pService_EPOCH->addCharacteristic(&pCharacteristicValuesStructureEpoch);
+	pCharacteristicValuesStructureEpoch.setCallbacks( new MyCallbacks2());
+
+	delay(250);
 
 	pService->start();
-	BLEAdvertising *pAdvertising = pServer->getAdvertising(); // this still is working for backward compatibility
-	//BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+	pService_EPOCH->start();
+	//BLEAdvertising *pAdvertising = pServer->getAdvertising(); // this still is working for backward compatibility
+	BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
 	pAdvertising->addServiceUUID(SERVICE_poolFloater_UUID);
+	pAdvertising->addServiceUUID(SERVICE_EPOCH_uuid);
 	pAdvertising->setScanResponse(true);
-	pAdvertising->setMinPreferred(0x06); // functions that help with iPhone connections issue
-	pAdvertising->setMinPreferred(0x12);
-	//pAdvertising->setMaxInterval(1000);
-	//pAdvertising->setMinInterval(2000);
-
+	//pAdvertising->setMinPreferred(0x06); // functions that help with iPhone connections issue
+	//pAdvertising->setMinPreferred(0x12);
+	pAdvertising->setMaxInterval(200);
+	pAdvertising->setMinInterval(100);
+	delay(500); //
+BLEDevice::startAdvertising();
 	//set some addvertisement nice data-------------------
 	//std::string manufacturerString = "IJ1968";
 	//BLEAdvertisementData advertisementData;
@@ -288,9 +366,9 @@ void setup() {
 
 	//pAdvertising->setAdvertisementData(advertisementData);
 	//set some addvertisement nice data-------------------------------------------------------
-	////esp_task_wdt_reset();
-	pAdvertising->start();
-	log_d("BLE Advertisement start");
+	esp_task_wdt_reset();
+	//pAdvertising->start();
+	log_i("BLE Advertisement start");
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 	//setup LCD setup LCD  setup LCD  setup LCD  setup LCD  setup LCD  setup LCD  setup LCD  setup LCD
@@ -308,27 +386,46 @@ void setup() {
 	printDeviceAddress();
 	////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-
+log_i("end of setup task---------------------------------------------------------");
 }
 
 void loop() {
+	log_i("start of loop task -----------------------------------------------------%s--", "GGGGGGGRRRRRR");
 	esp_task_wdt_reset();
 
 	log_d("memory report Free Heap %d, Free Sketch Space %d", ESP.getFreeHeap(),ESP.getFreeSketchSpace());
 //log_i("memory report Free Heap %d, Free Sketch Space %d", ESP.getFreeHeap(),ESP.getFreeSketchSpace());
+
 	delay(looptimedelay * 1000);
-	getTemperature(10);  //this function loads data into the structure from adc
+//	getTemperature(10);  //this function loads data into the structure from adc
+//Serial.printf("tempTop%f,temp_bot%f,batV%f,|t", boing.tempC, boing.tempCa, boing.battV);
+// Plot a sinus
+  Serial.print("temptop");
+  Serial.println(boing.tempC);
+ // Plot a sinus
+  Serial.print(">temptop:");
+  Serial.println( boing.tempC);
+
+  // Plot a cosinus
+  Serial.print(">tempbot:");
+  Serial.println( boing.tempCa);
+
+  // Plot a cosinus
+  Serial.print(">batV:");
+  Serial.println( boing.battV);
+
 
 	////esp_task_wdt_reset();
 	//do not store data if epoch has not been yet set- i.e less than 1.5 billion equivalent of july 14 2017
 	log_i("Just before if epoch > 2017 (1500000000) %Lu ", rtc.getEpoch());
 			
-	if (rtc.getEpoch() > 1500000000) {
-		//if (1) {
+	//if (rtc.getEpoch() > 1500000000) {
+		if (1) {
 		//if (epoch > 1) {
 			
 		bufferCircle.unshift(boing); //unshift will add data to the ringbuffer (boing)
 		log_i("Just after if epoch > 2017 (1500000000) %Lu ", rtc.getEpoch());
+		esp_task_wdt_reset();
 	}
 
 	//print time to serial port=======================================
@@ -342,11 +439,17 @@ void loop() {
 	// test connected
 	
 	if (isConnected) {
+		//if (1) {
 		log_i("inside isConnected-printBuffer llop /n my storage buffer has %d units stored of %d", bufferCircle.size(), bufferCircle.capacity);
+		esp_task_wdt_reset();
 		printBuffer(); //routine to disharge buffer to BLE and client
 	}
+	//BLEDevice::stopAdvertising();
+	delay(50);
 	//esp_bluedroid_disable;
 	//esp_bluedroid_deinit();
-	esp_deep_sleep_start();  //make it sleep deep
+	delay(100);
+	log_i("end of loop and sleep coming now------------------------------------------------------");
+	//esp_deep_sleep_start();  //make it sleep deep
 	//esp_light_sleep_start();  //make it sleep light
 }
